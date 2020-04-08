7f22daa12d47cb23438b264a19a0f88cac7cfda9
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-04-08 12:50:49 +0300

    Adding static serve for images

--- src/api/RunServer.ts
+++ src/api/RunServer.ts
@@ -3,12 +3,17 @@ import express from 'express';
 import bodyParser from "body-parser"
 import compression from "compression"
 
+import * as fs from 'fs'
+import * as path from 'path'
+
+
 
 export default () => {
     const app = express();
     app.use(bodyParser.urlencoded({ extended: true }))
     app.use(bodyParser.json())
     app.use("/api", compression())
+    app.use(express.static(__dirname.replace("/src/api", "") + '/uploads'))
     app.use("/api", router)
     console.log("Server started")
     return app.listen(process.env.PORT || 3000)
