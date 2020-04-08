8c862db580747812ef870da1455977d06c3c547e
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-04-08 12:50:15 +0300

    Long lat middleware restructuration

--- src/api/routerApi.ts
+++ src/api/routerApi.ts
@@ -9,25 +9,7 @@ import QuestionnaireController from './Controllers/QuestionnaireController';
 import PaysController from './Controllers/PaysController';
 import OptionController from './Controllers/OptionController';
 var router = express.Router()
-router.use("*", (req: Request, res: Response, next: NextFunction) => {
-    if (req.method != "POST" && req.method != "PUT") {
-        next()
-        return
-    }
-    var long = Object.keys(req.body as Object).filter((val: string) => /long[A-Z]/.test(val))
-    var lat = Object.keys(req.body as Object).filter((val: string) => /lat[A-Z]/.test(val))
-    for (const data of long) {
-        let suffix = data.substring(4)
-        let lo = req.body[data]
-        let la = req.body[`lat${suffix}`]
-        if (la == undefined || lo == undefined)
-            continue
-        delete req.body[data]
-        delete req.body[`lat${suffix}`]
-        req.body[`coord${suffix}`] = JSON.parse(`{"type":"Point","coordinates":[${la},${lo}]}`)
-    }
-    next()
-})
+router.use("*", convertLatLong())
 
 router.use("/centrals", new CentralController().mainRouter)
 router.use("/equipes", new EquipeController().mainRouter)
@@ -40,3 +22,25 @@ router.use("/reponses", new ReponseController().mainRouter)
 router.use("/signalements", new SignalementController().mainRouter)
 
 export default router;
+
+function convertLatLong() {
+    return (req: Request, res: Response, next: NextFunction) => {
+        if (req.method != "POST" && req.method != "PUT") {
+            next();
+            return;
+        }
+        var long = Object.keys(req.body as Object).filter((val: string) => /long[A-Z]/.test(val));
+        var lat = Object.keys(req.body as Object).filter((val: string) => /lat[A-Z]/.test(val));
+        for (const data of long) {
+            let suffix = data.substring(4);
+            let lo = req.body[data];
+            let la = req.body[`lat${suffix}`];
+            if (la == undefined || lo == undefined)
+                continue;
+            delete req.body[data];
+            delete req.body[`lat${suffix}`];
+            req.body[`coord${suffix}`] = JSON.parse(`{"type":"Point","coordinates":[${la},${lo}]}`);
+        }
+        next();
+    };
+}
