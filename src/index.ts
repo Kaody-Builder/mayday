9a37b45ea03248c57e5cd2f76b97d3e03027f68d
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-04-10 23:22:30 +0300

    Web socket broadcasting

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import 'reflect-metadata';
 import RunServer from './api/RunServer';
-import { MyWebSocket } from "./websocket/ws";
 import dotenv from 'dotenv'
 dotenv.config()
 const server = RunServer();
-const WebSocket = new MyWebSocket(server);
