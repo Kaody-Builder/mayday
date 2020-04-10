72ecb498c542f096ee44782bf445486ac4c82517
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-04-10 05:05:05 +0300

    Without tensor

--- src/api/Controllers/SymptomeController.ts
+++ src/api/Controllers/SymptomeController.ts
@@ -4,16 +4,16 @@ import * as tf from '@tensorflow/tfjs';
 
 
 export default class SymptomeController extends Controller {
-    model: tf.LayersModel
+    // model: tf.LayersModel
     constructor() {
         super()
         this.createConnectionAndAssignRepository()
     }
     async createConnectionAndAssignRepository(): Promise<void> {
         await this.addAllRoutes(this.mainRouter);
-        const tfn = require("@tensorflow/tfjs-node");
-        const handler = tfn.io.fileSystem("./model.json");
-        this.model = await tf.loadLayersModel(handler);
+        // const tfn = require("@tensorflow/tfjs-node");
+        // const handler = tfn.io.fileSystem("./model.json");
+        // this.model = await tf.loadLayersModel(handler);
         // this.model = await tf.loadLayersModel('http://localhost:9000/model.json');
     }
 
@@ -32,9 +32,9 @@ export default class SymptomeController extends Controller {
     }
     async postSymptome(router: Router) {
         router.post("/", async (req: Request, res: Response, next: NextFunction) => {
-            var data = [110.0552204,0,28,1,1]
-            var t = tf.tensor(data).reshape([1,5])
-            await this.sendResponse(res, 200, { data: this.model.predict(t).argMax().dataSync()[0]})
+            // var data = [110.0552204,0,28,1,1]
+            // var t = tf.tensor(data).reshape([1,5])
+            await this.sendResponse(res, 200, { data: "this.model.predict(t).argMax().dataSync()[0]"})
             next()
         })
     }
