517b60755ec6df4cd1b5102a4099ceb97ba4227a
rajaomariajaona <rajaomariajaona11@gmail.com>
2020-04-11 13:26:21 +0300

    sdf

--- src/api/Controllers/SymptomController.ts
+++ src/api/Controllers/SymptomController.ts
@@ -1,7 +1,7 @@
 import { Router, Response, Request, NextFunction, ErrorRequestHandler } from "express";
 import { Controller } from "../Controller";
 import * as tf from '@tensorflow/tfjs';
-
+import natural from "natural"
 
 export default class SymptomController extends Controller {
     // model: tf.LayersModel
@@ -10,7 +10,7 @@ export default class SymptomController extends Controller {
         this.createConnectionAndAssignRepository()
     }
     async createConnectionAndAssignRepository(): Promise<void> {
-         await this.addAllRoutes(this.mainRouter);
+        await this.addAllRoutes(this.mainRouter);
         // const tfn = require("@tensorflow/tfjs-node");
         // const handler = tfn.io.fileSystem("./model.json");
         // this.model = await tf.loadLayersModel(handler);
@@ -31,31 +31,39 @@ export default class SymptomController extends Controller {
     async addPut(router: Router): Promise<void> {
     }
     async postSymptom(router: Router) {
-        // router.post("/", async (req: Request, res: Response, next: NextFunction) => {
-        //     var text = req.body.text.toLowerCase()
-        //     var natural = require('natural');
-        //     var tokenizer = new natural.WordTokenizer();
-        //     var token = tokenizer.tokenize(text)
-        //     var key = ["fever", "cough", "breath", "pain", "chills", "sweating", "malaise", "cold", "pneumonia", "sore", "throat"]
-        //     var pt = 0
-        //     for(var t of token){
-        //         key.forEach(element => {
-        //             if(t.includes(element)){
-        //                 pt++
-        //             }
-        //             if(t == "t" || t== "not")
-        //                 pt--
-        //         });
-        //     }
-        //     var r = 0
-        //     if(token.length != 0)
-        //         r = (pt / token.length )
-        //     if (r > 1)
-        //         r = Math.random() * (0.95 - 0.85) + 0.85
-        //     if (r <= 0)
-        //         r = Math.random() * (0.4 - 0.05) + 0.05
-        //     await this.sendResponse(res, 201, { data: r })
-        //     next()
-        // })
+        router.post("/", async (req: Request, res: Response, next: NextFunction) => {
+            var text = req.body.text.toLowerCase()
+
+            var tokenizer = new natural.WordTokenizer();
+            var token = tokenizer.tokenize(text)
+            var key = ["fever", "cough", "breath", "pain", "chills", "sweating", "malaise", "cold", "pneumonia", "sore", "throat"]
+            var pt = 0
+            var q = 0
+            console.log(token)
+            for (let t of token) {
+                if(t.length > 4){
+                    q++ 
+                }
+                for (let element of key) {
+                    if (t.includes(element)) {
+                        pt++
+                    }
+                    
+                    if (t == "t" || t == "not"){
+                        pt--
+                        break
+                    }
+                }
+            }
+            var r = 0
+            if (token.length != 0)
+                r = (pt / q)
+            if (r > 1)
+                r = Math.random() * (0.95 - 0.85) + 0.85
+            if (r <= 0)
+                r = Math.random() * (0.4 - 0.05) + 0.05
+            await this.sendResponse(res, 201, { data: r })
+            next()
+        })
     }
 }
